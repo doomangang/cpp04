1b7c1e2e58d7d9452b9bbda8242a61e0d67d0801
doomangang <sp0943@cau.ac.kr>
2025-03-09 22:14:28 +0900

    04 mod

--- ex02/Brain.cpp
+++ ex02/Brain.cpp
@@ -22,7 +22,7 @@ Brain &Brain::operator=(const Brain &other) {
 	std::cout << "Brain assignment operator called\n";
 	if (this != &other) {
 		for (int i = 0; i < 100; i++)
-			ideas[i] = "Default Idea";
+			ideas[i] = other.ideas[i];
 	}
 	return (*this);
 }
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -15,10 +15,10 @@ public:
 	Character(const Character& other);
 	~Character();
 	Character&	operator=(const Character& other);
-	// std::string const & getName() const;
-	// void equip(AMateria* m);
-	// void unequip(int idx);
-	// void use(int idx, ICharacter& target);
+	std::string const & getName() const;
+	void equip(AMateria* m);
+	void unequip(int idx);
+	void use(int idx, ICharacter& target);
 };
 
 #endif
